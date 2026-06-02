0e7beb30af6babc9cb3fed48b6843931276e0272
theshanGeeth123 <theshangeethanjana@gmail.com>
2026-06-02 23:39:10 +0530

    Route Update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/products" />} />
         <Route path="/products" element={<ProductListPage />} />
         <Route path="/products/add" element={<AddProductPage />} />
         <Route path="/products/edit/:id" element={<EditProductPage />} />
